1427c1e20ebe2539434804873830910b048e3b25
Phing An Hsiao <phinganhsiao@gmail.com>
2021-12-09 16:39:04 -0500

    12/09`/21

--- client/src/App.js
+++ client/src/App.js
@@ -2,35 +2,13 @@ import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/Signup";
-<<<<<<< HEAD
-=======
 import Home from "./components/Home";
->>>>>>> a521b58ed9af76a5b6aa46ea0ae7f375a21a341f
 
 function App() {
 
   const [user, setUser] = useState(null)
 
   return (
-<<<<<<< HEAD
-    <BrowserRouter>
-      <div className="App">
-        
-
-          <Route path="/signup">
-            <SignUp />
-          </Route>
-
-          <Route path="/login">
-            <h1>Page Count: {count}</h1>
-            <Login />
-          </Route>
-
-          <Route path="/"></Route>
-
-      </div>
-    </BrowserRouter>
-=======
     <div className="App">
     
       <main>
@@ -55,7 +33,6 @@ function App() {
             )}
       </main>
     </div>
->>>>>>> a521b58ed9af76a5b6aa46ea0ae7f375a21a341f
   );
 }
 
