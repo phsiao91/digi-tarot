61dc53ee09025352a46f9605ab07bc996f0849b3
McKayla Lankau <lankaukk@yahoo.com>
2021-12-09 20:55:00 -0500

    successfully select 3 random cards

--- client/src/components/pages/Welcome.js
+++ client/src/components/pages/Welcome.js
@@ -5,57 +5,50 @@ import React, {useState, useEffect} from "react";
 function Welcome({user}) {
 
     const [card, setCard] = useState([])
-    // state = {
-    //     cards: []
-    // }
+    const [threeCards, setThreeCards] = useState([])
 
     const fetchCards = () => { 
-        console.log("magic fortune!");
+        console.log("fetched!");
 
         fetch("/card")
-
-        .then(response => response.json())
-
-        .then(result => setCard(result)
-          )
-
-        .catch(err => console.log(err))
+            .then(response => response.json())
+            .then(result => setCard(result),
+                console.log(card)
+            )
+            .catch(err => console.log(err))
       }
-      useEffect(fetchCards, [])
-    // componentDidMount() {
-    //     this.fetchCards()
-    // }
-
-    // const getQuote = () => {
-    //     fetch("https://type.fit/api/quotes")
-    //     .then(res => res.json())
-    //     .then(fetchedQuote => {console.log(fetchedQuote)
-    //         let randomQuote = fetchedQuote[Math.floor(Math.random() * fetchedQuote.length)]
-
-    //     setQuote(randomQuote)
-
-    // })
-    // }
-    // useEffect(getQuote, [])
-
-    // fetchCards()
-
-    console.log(card)
-
-    const selectCards = () => {
-        console.log("magic fortune!");
-
-    };
-
     
+    useEffect(fetchCards, [])
+
+    const getThreeCards = () => {
+        let threeCards = [];
+        while(threeCards.length < 3){
+            let newCard = card[Math.floor(Math.random() * 34) + 1];
+            if(threeCards.indexOf(newCard) === -1) threeCards.push(newCard);
+        }
+        // console.log(threeCards);
+        setThreeCards(threeCards)
+    }
+
+    console.log(threeCards);
+
     return (
         <div>
             <h4 className="welcome"> Welcome, {user.username}</h4>
 
-            <button onClick={() => fetchCards()}>
+            <button onClick={() => getThreeCards()}>
                 Get a Reading
             </button>
+
+            <div className="card-layout-container">
+                <img classname="card-picked" src={threeCards[0].image}></img>
+                <img classname="card-picked" src={threeCards[1].image}></img>
+                <img classname="card-picked" src={threeCards[2].image}></img>
+            </div>
+
         </div>
+
+       
     )
 }
 
