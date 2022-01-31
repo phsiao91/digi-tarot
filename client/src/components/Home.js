f2e75d0db7423efa3ca61d85424ca8573b7106ed
Phing An Hsiao <phinganhsiao@gmail.com>
2022-01-31 13:00:35 -0500

    01/31/22

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -23,13 +23,47 @@ function Home({ user }) {
         </div>)
     } else {
       return (
-        <div>
-            <div className="prompt">
-                <h3 className="prompt_2" onClick={login}>Login </h3>
-                <h3 className="prompt_3">or </h3>
-                <h3 className="prompt_4" onClick={signup}>SignUp</h3>
+        // <div>
+        //     <div className="prompt">
+        //         <h3 className="prompt_2" onClick={login}>Login </h3>
+        //         <h3 className="prompt_3">or </h3>
+        //         <h3 className="prompt_4" onClick={signup}>SignUp</h3>
+        //     </div>
+        // </div>
+        <div class="container">
+        <div class="screen">
+          <div class="screen__content">
+            <form class="login">
+              <div class="login__field">
+                <i class="login__icon fas fa-user"></i>
+                <input type="text" class="login__input" placeholder="Username / Email"/>
+              </div>
+              <div class="login__field">
+                <i class="login__icon fas fa-lock"></i>
+                <input type="password" class="login__input" placeholder="Password"/>
+              </div>
+              <div class="login__field">
+                <i class="login__icon fas fa-lock"></i>
+                <input type="password" class="login__input" placeholder="Confirm Password"/>
+              </div>
+              <button class="button login__submit">
+                <span class="button__text">Create Account</span>
+                <i class="button__icon fas fa-chevron-right"></i>
+              </button>				
+            </form>
+            <div class="social-login" onClick={login}>
+              <h4>Already have an account?</h4>
+              <h4>Login here</h4>
             </div>
+          </div>
+          <div class="screen__background">
+            <span class="screen__background__shape screen__background__shape4"></span>
+            <span class="screen__background__shape screen__background__shape3"></span>		
+            <span class="screen__background__shape screen__background__shape2"></span>
+            <span class="screen__background__shape screen__background__shape1"></span>
+          </div>		
         </div>
+      </div>
       )
     }
   }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -26,38 +26,66 @@ function Login( {setUser} ) {
 
 
     return (
-        <div>
-      <div className="card">
-                <h2>Login</h2>
-                <div className="row">
-                    <div className="col">
-                        <div className="form-group">
-                            <label htmlFor="username">username</label>
-                            <input 
-                                type="text"
-                                name="username"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}/>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="form-group">
-                            <label htmlFor="password">password</label>
-                            <input 
-                                type="password"
-                                name="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}/>
-                        </div>
-                    </div>
-                    <Link to="welcome">
-                    <input type="submit" className="button" onClick={handleLogin} value="Login"></input>
-                    </Link>
-                </div>
-                <Link to='signup'>Don't have an account? Sign Up!!</Link>
-            </div>
+    //     <div>
+    //   <div className="card">
+    //             <h2>Login</h2>
+    //             <div className="row">
+    //                 <div className="col">
+    //                     <div className="form-group">
+    //                         <label htmlFor="username">username</label>
+    //                         <input 
+    //                             type="text"
+    //                             name="username"
+    //                             value={username}
+    //                             onChange={(e) => setUsername(e.target.value)}/>
+    //                     </div>
+    //                 </div>
+    //                 <div className="col">
+    //                     <div className="form-group">
+    //                         <label htmlFor="password">password</label>
+    //                         <input 
+    //                             type="password"
+    //                             name="password"
+    //                             value={password}
+    //                             onChange={(e) => setPassword(e.target.value)}/>
+    //                     </div>
+    //                 </div>
+    //                 <Link to="welcome">
+    //                 <input type="submit" className="button" onClick={handleLogin} value="Login"></input>
+    //                 </Link>
+    //             </div>
+    //             <Link to='signup'>Don't have an account? Sign Up!!</Link>
+    //         </div>
       
-    </div>
+    // </div>
+    <div class="container">
+        <div class="screen">
+          <div class="screen__content">
+            <form class="login">
+              <div class="login__field">
+                <i class="login__icon fas fa-user"></i>
+                <input type="text" class="login__input" placeholder="Username / Email"/>
+              </div>
+              <div class="login__field">
+                <i class="login__icon fas fa-lock"></i>
+                <input type="password" class="login__input" placeholder="Password"/>
+              </div>
+              <button class="button login__submit">
+                <span class="button__text">Login</span>
+                <i class="button__icon fas fa-chevron-right"></i>
+              </button>				
+            </form>
+            <div class="social-login">
+            </div>
+          </div>
+          <div class="screen__background">
+            <span class="screen__background__shape screen__background__shape4"></span>
+            <span class="screen__background__shape screen__background__shape3"></span>		
+            <span class="screen__background__shape screen__background__shape2"></span>
+            <span class="screen__background__shape screen__background__shape1"></span>
+          </div>		
+        </div>
+      </div>
     )
 }
 
