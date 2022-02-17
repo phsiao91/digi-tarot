869a98fb99dd86885b3741e90c528c3a49e190f2
Phing An Hsiao <phinganhsiao@gmail.com>
2022-02-17 00:00:01 -0500

    16/2

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Link } from "react-router-dom"
-import {  Switch } from "react-router-dom";
+// import { Link } from "react-router-dom"
+// import {  Switch } from "react-router-dom";
 // import { useState, useEffect } from "react";
 
 function NavBar({user, setUser}) {
